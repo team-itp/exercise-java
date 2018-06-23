4476c3b7313fb57840336cfdc9600574a2a3fc82
Yuto Nagano <chamelion@hotmail.co.jp>
2018-06-23 16:27:59 +0900

    excercise2 作成

---
+++ excercise2/src/main/java/excercise2/App.java
@@ -0,0 +1,14 @@
+package excercise2;
+
+/**
+ * Hello world! を 5 回繰り返す
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        for (int i = 0; i < 5; i++) {
+            System.out.println( "Hello World!" );
+        }
+    }
+}
