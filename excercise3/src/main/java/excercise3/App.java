5c91d820a141041bf24727070ff9b3cb290f5f3c
Yuto Nagano <chamelion@hotmail.co.jp>
2018-06-23 17:04:23 +0900

    フォーマット見直し

--- excercise3/src/main/java/excercise3/App.java
+++ excercise3/src/main/java/excercise3/App.java
@@ -8,8 +8,7 @@ import java.io.InputStreamReader;
  * コンソールから文字列を読み取ってそれを表示する
  */
 public class App {
-    public static void main(String[] args)
-        throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("文字列を入力してください: ");
         String input = reader.readLine();
